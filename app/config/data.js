d85df5e565dcb169450f58f4683acba732c7fbf8
kwishnu <kwish1777@gmail.com>
2017-06-25 09:58:00 -0700

    Changed Side Menu properties

--- app/config/data.js
+++ app/config/data.js
@@ -1,155 +1,173 @@
 module.exports = [
     {
-		title: 'Contents',
+		title: 'Home',
 		index: '0',
-		type: 'toVerses',
+		type: 'home',
         link: 'home'
     },
     {
-		title: 'Today\'s Verse',
+		title: 'Our Mission',
 		index: '1',
-		type: 'toVerses',
-        link: 'game'
+		type: 'home',
+        link: 'mission'
     },
     {
-		title: 'Daily Verses',
+		title: 'How To Play',
 		index: '2',
-		type: 'toVerses',
-        link: 'daily'
+		type: 'home',
+        link: 'intro'
     },
     {
-		title: 'Easy',
+		title: 'Settings',
 		index: '3',
-		type: 'store',
+		type: 'home',
+        link: 'settings'
+    },
+    {
+		title: 'About',
+		index: '4',
+		type: 'about',
+        link: 'about'
+    },
+    {
+		title: 'Verse Collections',
+		index: '5',
+		type: 'donate',
         link: 'store',
         data:[
                 {
                 name: 'Inspiration',
-                product_id: 'fm.e.capri.50',
-                price: '$1.99',
+                product_id: 'rv.verse.collection.inspiration',
                 show: 'true',
                 num_verses: '50',
-                difficulty: 'E',
-                color: '#831d1d',
+                color: '#85c613',
                 },
                 {
                 name: 'Forgiveness',
-                product_id: 'fm.e.chartreuse.50',
-                price: '$1.99',
+                product_id: 'rv.verse.collection.forgiveness',
                 show: 'true',
                 num_verses: '50',
-                difficulty: 'E',
-                color: '#1a4bb0'//'#DFFF00',
-                }
-            ]
-    },
-    {
-		title: 'Moderate',
-		index: '4',
-		type: 'store',
-        link: 'store',
-        data:[
-                {
-                name: 'Friendship',
-                product_id: 'fm.m.bungalow.50',
-                price: '$1.99',
-                show: 'true',
-                num_verses: '50',
-                difficulty: 'M',
-                color: '#447054',
+                color: '#1a4bb0'
                 },
                 {
-                name: 'Hope',
-                product_id: 'fm.m.cruise.50',
-                price: '$1.99',
-                show: 'true',
-                num_verses: '50',
-                difficulty: 'M',
-                color: '#b6b00f',
-                }
-            ]
-    },
-    {
-		title: 'Hard',
-		index: '5',
-		type: 'store',
-        link: 'store',
-        data:[
-                {
                 name: 'Love',
-                product_id: 'fm.h.blizzard.50',
-                price: '$1.99',
+                product_id: 'rv.verse.collection.love',
                 show: 'true',
                 num_verses: '50',
-                difficulty: 'H',
-                color: '#983a7a',
+                color: '#831d1d',
                 },
                 {
                 name: 'Strength',
-                product_id: 'fm.h.cyclone.50',
-                price: '$1.99',
+                product_id: 'rv.verse.collection.strength',
                 show: 'true',
                 num_verses: '50',
-                difficulty: 'H',
+                color: '#7385a5'
+                },
+                {
+                name: 'Hope',
+                product_id: 'rv.verse.collection.hope',
+                show: 'true',
+                num_verses: '50',
+                color: '#77f170',
+                },
+                {
+                name: 'Peace',
+                product_id: 'rv.verse.collection.peace',
+                show: 'true',
+                num_verses: '50',
+                color: '#dba8ee'
+                }
+            ]
+    },
+    {
+		title: 'Old Testament',
+		index: '6',
+		type: 'donate',
+        link: 'store',
+        data:[
+                {
+                name: 'Genesis',
+                product_id: 'rv.verse.book.genesis',
+                show: 'true',
+                color: '#77c019',
+                },
+                {
+                name: 'Exodus',
+                product_id: 'rv.verse.book.exodus',
+                show: 'true',
+                color: '#552b0c',
+                },
+                {
+                name: 'Leviticus',
+                product_id: 'rv.verse.book.leviticus',
+                show: 'true',
+                color: '#448150',
+                },
+                {
+                name: 'Numbers',
+                product_id: 'rv.verse.book.numbers',
+                show: 'true',
+                color: '#3e137a',
+                },
+                {
+                name: 'Deuteronomy',
+                product_id: 'rv.verse.book.deuteronomy',
+                show: 'true',
+                color: '#2d1969',
+                }
+            ]
+    },
+    {
+		title: 'New Testament',
+		index: '7',
+		type: 'donate',
+        link: 'store',
+        data:[
+                {
+                name: 'Matthew',
+                product_id: 'rv.verse.book.matthew',
+                show: 'true',
+                color: '#1780ec',
+                },
+                {
+                name: 'Mark',
+                product_id: 'rv.verse.book.mark',
+                show: 'true',
+                color: '#6d920a',
+                },
+                {
+                name: 'Luke',
+                product_id: 'rv.verse.book.luke',
+                show: 'true',
+                color: '#905f93',
+                },
+                {
+                name: 'John',
+                product_id: 'rv.verse.book.john',
+                show: 'true',
                 color: '#107088',
                 }
             ]
     },
     {
-		title: 'Theme',
-		index: '6',
-		type: 'store',
+		title: 'Hint Packages',
+		index: '8',
+		type: 'donate',
+        link: 'store',
+    },
+    {
+		title: 'Value Combinations',
+		index: '9',
+		type: 'donate',
         link: 'store',
         data:[
-                {
-                name: 'Peace',
-                product_id: 'fm.t.art_and_literature.50',
-                price: '$1.99',
-                show: 'true',
-                num_verses: '50',
-                difficulty: 'H',
-                color: '#074f3d',
-                },
-                {
-                name: 'Encouragement',
-                product_id: 'fm.t.food_and_drink.50',
-                price: '$1.99',
-                show: 'true',
-                num_verses: '50',
-                difficulty: 'H',
-                color: '#4033c0',
-                }
             ]
     },
     {
-		title: 'Easy Combo',
-		index: '7',
-		type: 'combos',
-        link: 'store3',
-        data:[
-            ]
-    },
-    {
-		title: 'Moderate Combo',
-		index: '8',
-		type: 'combos',
-        link: 'store3',
-        data:[
-            ]
-    },
-    {
-		title: 'Hard Combo',
-		index: '9',
-		type: 'combos',
-        link: 'store3',
-        data:[
-            ]
-    },
-    {
-		title: 'Mixed Combo',
+		title: 'Popular',
 		index: '10',
-		type: 'combos',
-        link: 'store3',
+		type: 'donate',
+        link: 'store',
         data:[
             ]
     },
@@ -165,27 +183,9 @@ module.exports = [
 		type: 'social',
         link: 'twitter'
     },
-    {
-		title: 'How To Play',
-		index: '13',
-		type: 'toVerses',
-        link: 'app_intro'
-    },
-    {
-		title: 'Settings',
-		index: '14',
-		type: 'about',
-        link: 'settings'
-    },
-    {
-		title: 'About',
-		index: '15',
-		type: 'about',
-        link: 'about'
-    },
 	{
-		title: 'Today\'s Verse',
-		index: '16',
+		title: 'Verse of the Day',
+		index: '13',
 		type: 'daily',
 		show: 'true',
 		num_verses: '1',
@@ -193,12 +193,12 @@ module.exports = [
         product_id: '',
 		bg_color: '#055105',
 		verses:[
-            `Genesis 1:1-2**In the beginning God created the heavens and the earth. Now the earth was formless and empty, darkness was over the surface of the deep, and the Spirit of God was hovering over the waters.`
+            `2**Genesis 1:1-2**In the beginning God created the heavens and the earth. Now the earth was formless and empty, darkness was over the surface of the deep, and the Spirit of God was hovering over the waters.`
         ]
 	},
 	{
 		title: 'Last Three Days',
-		index: '17',
+		index: '14',
 		type: 'daily',
 		show: 'true',
 		num_verses: '3',
@@ -206,14 +206,14 @@ module.exports = [
         product_id: '',
 		bg_color: '#795959',
 		verses:[
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`
+            `2**John 3:16**For God so loved the world, that he gave his one and only Son, that whoever believes in him should not perish, but have eternal life.`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`
         ]
 	},
 	{
 		title: 'Last Thirty Days',
-		index: '18',
+		index: '15',
 		type: 'daily',
 		show: 'false',
 		num_verses: '30',
@@ -221,41 +221,41 @@ module.exports = [
         product_id: '',
 		bg_color: '#FFB300',
 		verses:[
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`
         ]
 	},
 	{
 		title: 'In The Beginning...',
-		index: '19',
+		index: '16',
 		type: 'mypack',
 		show: 'true',
 		num_verses: '30',
@@ -264,46 +264,73 @@ module.exports = [
         product_id: '',
 		bg_color: '#2B0B30',
 		verses:[
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
-            `John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`,
+            `2**John 3:16**For God so loved the world, that he gave`
         ]
+	},
+	{
+		title: 'Favorites',
+		index: '17',
+		type: 'mypack',
+		show: 'false',
+		num_verses: '0',
+		num_solved: '0',
+		solved: [],
+        product_id: '',
+		bg_color: '#b21111',
+		verses:[
+        ]
+	},
+	{
+		title: '',
+		index: '18',
+		type: 'forsale',
+        product_id: '',
+		show: 'true',
+		bg_color: '#000000'
+	},
+	{
+		title: '',
+		index: '19',
+		type: 'forsale',
+        product_id: '',
+		show: 'true',
+		bg_color: '#000000'
 	},
 	{
 		title: '',
 		index: '20',
 		type: 'forsale',
         product_id: '',
-        price: '',
 		show: 'true',
-		num_verses: '',
 		bg_color: '#000000'
 	},
 	{
@@ -311,29 +338,7 @@ module.exports = [
 		index: '21',
 		type: 'forsale',
         product_id: '',
-        price: '',
 		show: 'true',
-		num_verses: '',
-		bg_color: '#000000'
-	},
-	{
-		title: '',
-		index: '22',
-		type: 'forsale',
-        product_id: '',
-        price: '',
-		show: 'true',
-		num_verses: '',
-		bg_color: '#000000'
-	},
-	{
-		title: '',
-		index: '23',
-		type: 'forsale',
-        product_id: '',
-        price: '',
-		show: 'true',
-		num_verses: '',
 		bg_color: '#000000'
 	}
 ];
